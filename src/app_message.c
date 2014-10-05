b44bd271a51a9780ba9256aa0e06cff06b881454
Andrew Jazbec <andrew.jazbec@gmail.com>
2014-10-05 10:31:31 -0700

    code

---
+++ src/app_message.c
@@ -0,0 +1,155 @@
+#include <pebble.h>
+  
+#define NUM_MENU_SECTIONS 1
+#define NUM_FIRST_MENU_ITEMS 2
+
+Window *window;	
+
+static MenuLayer *menu_layer;
+
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return NUM_MENU_SECTIONS;
+}
+
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  switch (section_index) {
+    case 0:
+      return NUM_FIRST_MENU_ITEMS;
+    default:
+      return 0;
+  }
+}
+
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  switch (section_index) {
+    case 0:
+      menu_cell_basic_header_draw(ctx, cell_layer, "PBW Files");
+      break;
+  }
+}
+
+// This is the menu item draw callback where you specify what each item should look like
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  // Determine which section we're going to draw in
+  switch (cell_index->section) {
+    case 0:
+      // Use the row to specify which item we'll draw
+      switch (cell_index->row) {
+        case 0:
+          // This is a basic menu item with a title and subtitle
+          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "", NULL);
+          break;
+
+        case 1:
+          // This is a basic menu item with a title and subtitle
+          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "", NULL);
+          break;
+      }
+      break;
+  }
+}
+
+void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data)
+{
+    int which = cell_index->row;
+  
+    if(which == 0) {
+    }
+  
+    if(which == 1) {
+    }
+}  
+  
+  
+  
+	
+// Key values for AppMessage Dictionary
+enum {
+	STATUS_KEY = 0,	
+	MESSAGE_KEY = 1
+};
+
+// Write message to buffer & send
+void send_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_uint8(iter, STATUS_KEY, 0x1);
+	
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}
+
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *tuple;
+	
+	tuple = dict_find(received, STATUS_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
+	}
+	
+	tuple = dict_find(received, MESSAGE_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
+	}}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {	
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+}
+
+void init(void) {
+	window = window_create();
+  
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+  
+  menu_layer = menu_layer_create(bounds);
+  
+  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+    .draw_row = menu_draw_row_callback,
+    .select_click = menu_select_callback,
+  });
+  
+  // Bind the menu layer's click config provider to the window for interactivity
+  menu_layer_set_click_config_onto_window(menu_layer, window);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
+  
+  //window_set_click_config_provider(window, click_config_provider);
+  
+	window_stack_push(window, true);
+	
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+		
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	
+	send_message();
+}
+
+void deinit(void) {
+	app_message_deregister_callbacks();
+	window_destroy(window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
